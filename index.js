b59f15c564b64ef96523750df4e1aa13d3624a19
ambertua <amber.tuamoheloa@yahoo.com>
2023-03-27 18:11:38 -0700

    process

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const pacman = document.querySelector(".pacman");
+const character = newImage('Assets/pacright.png')
 
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = ''
+    }
+    if(direction === 'west'){
+        character.src = ''
+    }
+    if(direction === 'north'){
+        character.src = ''
+    }
+    if(direction === 'east'){
+        character.src = ''
+    }
+    if(direction === 'south'){
+        character.src = ''
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
